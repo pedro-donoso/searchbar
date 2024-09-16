3a1913081e64876ddb77043d1f844803c297e5d2
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-16 13:37:30 -0300

    creo SearchBar

--- app/src/main/java/com/example/searchbar/MainActivity.kt
+++ app/src/main/java/com/example/searchbar/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.searchbar
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
@@ -13,15 +15,19 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.searchbar.ui.theme.SearchbarTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SearchbarTheme {
+                val ctx = LocalContext.current
+
                 Surface (
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -29,10 +35,14 @@ class MainActivity : ComponentActivity() {
                     SearchBar(
                         query = "Search",
                         onQueryChange = {},
-                        onSearch = {},
-                        active = false,
+                        onSearch = {
+                            Toast.makeText(ctx, "Search", Toast.LENGTH_SHORT).show()
+                        },
+                        active = true,
                         onActiveChange = {}
-                    )
+                    ){
+
+                    }
                 }
             }
         }
