1647dbc8c427716b8a0b744626456ddfb885839f
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-16 13:57:41 -0300

    agrego lista de paises

--- app/src/main/java/com/example/searchbar/MainActivity.kt
+++ app/src/main/java/com/example/searchbar/MainActivity.kt
@@ -6,40 +6,46 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.searchbar.ui.theme.SearchbarTheme
 
+// clase principal
 class MainActivity : ComponentActivity() {
+    // funcion principal
     @OptIn(ExperimentalMaterial3Api::class)
+    // creacion de la interfaz
     override fun onCreate(savedInstanceState: Bundle?) {
+        // funcion para crear la interfaz
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            // tema de la interfaz
             SearchbarTheme {
+                // superficie de la interfaz
                 val ctx = LocalContext.current
-
-                Surface (
+                Surface(
+                    // tamaño de la superficie
                     modifier = Modifier.fillMaxSize(),
+                    // color de la superficie
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    // barra de busqueda
                     var query by remember { mutableStateOf("") }
+                    // estado de la barra de busqueda
                     var active by remember { mutableStateOf(false) }
 
+                    // interfaz de la barra de busqueda
                     SearchBar(
+                        // modificadores de la barra de busqueda
                         query = query,
                         onQueryChange = { query = it },
                         onSearch = {
@@ -48,8 +54,210 @@ class MainActivity : ComponentActivity() {
                         },
                         active = active,
                         onActiveChange = { active = it }
-                    ){
+                    ) {
+                        /* List of name of the countries in the world order alphabetically.
+                         */
 
+                        val countries = listOf(
+                            "Afghanistan",
+                            "Albania",
+                            "Algeria",
+                            "Andorra",
+                            "Angola",
+                            "Antigua and Barbuda",
+                            "Argentina",
+                            "Ar menia",
+                            "Australia",
+                            "Austria",
+                            "Azerbaijan",
+                            "Bahamas",
+                            "Bahrain",
+                            "Bangladesh",
+                            "Barbados",
+                            "Belarus",
+                            "Belgium",
+                            "Belize",
+                            "Benin",
+                            "Bhutan",
+                            "Bolivia",
+                            "Bosnia and Herzegovina",
+                            "Botswana",
+                            "Brazil",
+                            "Brunei",
+                            "Bulgaria",
+                            "Burkina Faso",
+                            "Burundi",
+                            "Cambodia",
+                            "Cameroon",
+                            "Canada",
+                            "Central African Republic",
+                            "Chad",
+                            "Chile",
+                            "China",
+                            "Colombia",
+                            "Comoros",
+                            "Congo (Congo-Brazzaville)",
+                            "Costa Rica",
+                            "Croatia",
+                            "Cuba",
+                            "Cyprus",
+                            "Czechia (Czech Republic)",
+                            "Democratic Republic of the Congo",
+                            "Denmark",
+                            "Djibouti",
+                            "Dominica",
+                            "Dominican Republic",
+                            "Ecuador",
+                            "Egypt",
+                            "El Salvador",
+                            "Equatorial Guinea",
+                            "Eritrea",
+                            "Estonia",
+                            "Ethiopia",
+                            "Fiji",
+                            "Finland",
+                            "France",
+                            "Gabon",
+                            "Gambia",
+                            "Georgia",
+                            "Germany",
+                            "Ghana",
+                            "Greece",
+                            "Greenland",
+                            "Grenada",
+                            "Guatemala",
+                            "Guinea",
+                            "Guinea-Bissau",
+                            "Guyana",
+                            "Haiti",
+                            "Honduras",
+                            "Hong Kong",
+                            "Hungary",
+                            "Iceland",
+                            "India",
+                            "Indonesia",
+                            "Iran",
+                            "Iraq",
+                            "Ireland",
+                            "Israel",
+                            "Italy",
+                            "Jamaica",
+                            "Japan",
+                            "Jordan",
+                            "Kazakhstan",
+                            "Kenya",
+                            "Kiribati",
+                            "North Korea",
+                            "South Korea",
+                            "Kosovo",
+                            "Kuwait",
+                            "Kyrgyzstan",
+                            "Laos",
+                            "Latvia",
+                            "Lebanon",
+                            "Lesotho",
+                            "Liberia",
+                            "Libya",
+                            "Liechtenstein",
+                            "Lithuania",
+                            "Luxembourg",
+                            "Macedonia",
+                            "Madagascar",
+                            "Malawi",
+                            "Malaysia",
+                            "Maldives",
+                            "Mali",
+                            "Malta",
+                            "Marshall Islands",
+                            "Mauritania",
+                            "Mauritius",
+                            "Mexico",
+                            "Micronesia",
+                            "Moldova",
+                            "Monaco",
+                            "Mongolia",
+                            "Montenegro",
+                            "Morocco",
+                            "Mozambique",
+                            "Myanmar",
+                            "Namibia",
+                            "Nauru",
+                            "Nepal",
+                            "Netherlands",
+                            "New Zealand",
+                            "Nicaragua",
+                            "Niger",
+                            "Nigeria",
+                            "Northern Mariana Islands",
+                            "Norway",
+                            "Oman",
+                            "Pakistan",
+                            "Palau",
+                            "Palestine, State of",
+                            "Panama",
+                            "Papua New Guinea",
+                            "Paraguay",
+                            "Peru",
+                            "Philippines",
+                            "Poland",
+                            "Portugal",
+                            "Puerto Rico",
+                            "Qatar",
+                            "Romania",
+                            "Russia",
+                            "Rwanda",
+                            "Saint Kitts and Nevis",
+                            "Saint Lucia",
+                            "Saint Vincent and the Grenadines",
+                            "Samoa",
+                            "San Marino",
+                            "Sao Tome and Principe",
+                            "Saudi Arabia",
+                            "Senegal",
+                            "Serbia",
+                            "Seychelles",
+                            "Sierra Leone",
+                            "Singapore",
+                            "Sint Maarten",
+                            "Slovakia",
+                            "Slovenia",
+                            "Solomon Islands",
+                            "Somalia",
+                            "South Africa",
+                            "Spain",
+                            "Sri Lanka",
+                            "Sudan",
+                            "Sudan, South",
+                            "Suriname",
+                            "Swaziland",
+                            "Sweden",
+                            "Switzerland",
+                            "Syria",
+                            "Taiwan",
+                            "Tajikistan",
+                            "Tanzania",
+                            "Thailand",
+                            "Togo",
+                            "Tonga",
+                            "Trinidad and Tobago",
+                            "Tunisia",
+                            "Turkey",
+                            "Turkmenistan",
+                            "Tuvalu",
+                            "Uganda",
+                            "Ukraine",
+                            "United Arab Emirates",
+                            "United Kingdom",
+                            "United States of America",
+                            "Uruguay",
+                            "Uzbekistan",
+                            "Vanuatu",
+                            "Venezuela",
+                            "Vietnam",
+                            "Yemen",
+                            "Zambia",
+                            "Zimbabwe"
+                        )
                     }
                 }
             }
